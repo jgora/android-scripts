464d19306d529463332a5fd726683854c217e201
jgora <35234089+jgora@users.noreply.github.com>
2020-02-11 18:32:22 +0100

    Create whatsapp-battery-friendly.sh

---
+++ whatsapp-battery-friendly.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+
+pm disable com.whatsapp/com.whatsapp.gcm.RegistrationIntentService
+
+pm disable com.whatsapp/com.whatsapp.memory.dump.MemoryDumpUploadService
+
+pm disable com.whatsapp/com.whatsapp.breakpad.NativeCrashDumpUploadService
