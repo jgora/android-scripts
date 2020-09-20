cbbf55dc79bfe1ffa6a3f2d2c85a362ba48e35da
jgora <35234089+jgora@users.noreply.github.com>
2020-09-20 11:29:25 +0200

    Update whatsapp-battery-friendly.sh

--- whatsapp-battery-friendly.sh
+++ whatsapp-battery-friendly.sh
@@ -5,3 +5,7 @@ pm disable com.whatsapp/com.whatsapp.gcm.RegistrationIntentService
 pm disable com.whatsapp/com.whatsapp.memory.dump.MemoryDumpUploadService
 
 pm disable com.whatsapp/com.whatsapp.breakpad.NativeCrashDumpUploadService
+
+pm disable com.whatsapp/com.whatsapp.push.RegistrationIntentService
+
+pm disable com.whatsapp/com.whatsapp.util.crash.JavaCrashesUploadService
